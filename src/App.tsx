0d3bd20f7270df2f85b8eebaf82378fa68bd37f9
jesseb34r <jesseb34r@jesseaubin.me>
2025-02-02 11:42:54 -0800

    work on styling home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import HomePage from "./pages/home_page";
 
 function App() {
   return (
-    <main
-      id="main"
-      class="flex flex-col items-center min-h-screen justify-center bg-gray-app p-20 text-gray-normal"
-    >
+    <div class="min-h-screen bg-gray-app text-gray-normal">
       <HomePage />
-    </main>
+    </div>
   );
 }
 
--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,17 +1,17 @@
 export default function HomePage() {
   return (
-    <>
-      <h1 class="text-teal-10 text-4xl mb-6">Moxcel</h1>
+    <main class="flex flex-col pt-10 mx-auto w-[80%]">
+      <h1 class="text-teal-10 text-4xl mb-6 text-center">Moxcel</h1>
       <div class="flex flex-col gap-2">
-        <div class="flex gap-20 items-end">
-          <h2>Decks</h2>
-          <button type="button" class="bg-grass-action border-grass-normal px-2 py-1 rounded">
+        <div class="flex gap-20 items-end justify-between">
+          <h2 class="text-xl text-gray-dim">Decks</h2>
+          <button type="button" class="bg-grass-action px-2 py-1 rounded">
             New
           </button>
         </div>
-        <hr />
+        <hr class="text-gray-dim" />
         <div>All my decks</div>
       </div>
-    </>
+    </main>
   );
 }
