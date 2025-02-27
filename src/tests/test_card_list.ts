2fbc669922725adb65d94f7bcb56c96d2d1e923c
jesseb34r <jesseb34r@jesseaubin.me>
2025-02-27 14:27:20 -0800

    comment test_card_list.ts

--- src/tests/test_card_list.ts
+++ src/tests/test_card_list.ts
@@ -1,24 +1,25 @@
-import { list_parse_from_string } from "../card_list";
+import { list_parse_from_string } from "../utils/card_list";
 
 console.log("Starting test...");
 test_list_parsing()
   .then(() => console.log("Test completed"))
   .catch((err) => console.error("Test failed:", err));
 
+// Tests list parsing and fetching from the Scryfall API. Currently just a manual check, probably
+// need to implement actual testing at some point to confirm that I don't break shit.
 async function test_list_parsing() {
   const sample_card_list = `
-    # Mainboard
-    4x Lightning Bolt
-    3 Birds of Paradise
-    Island
-    2x Black Lotus
-  `;
+# Mainboard
+4x Lightning Bolt
+3 Birds of Paradise
+Island
+2x Black Lotus`;
 
   try {
     console.log("Parsing card list...");
     const parsed_list = await list_parse_from_string(sample_card_list);
     console.log("Successfully parsed list:");
-    // console.log(JSON.stringify(parsed_list, null, 2));
+    console.log(JSON.stringify(parsed_list, null, 2));
     console.log(`Found ${parsed_list.list.length} unique cards`);
 
     // Count total cards
