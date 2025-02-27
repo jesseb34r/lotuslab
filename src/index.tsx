75c871bbf00359f750d8d668492c1a69b96e21fa
jesseb34r <jesseb34r@jesseaubin.me>
2025-02-27 14:16:45 -0800

    use @solid/router for navigation and page management

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import "./index.css";
-import App from "./App.tsx";
+import { Router, Route } from "@solidjs/router";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+import "./index.css";
+
+import { HomePage } from "./pages/home_page";
+import { ListPage } from "./pages/list_page";
+import type { ParentComponent } from "solid-js";
+
+const AppLayout: ParentComponent = (props) => (
+  <div class="min-h-screen bg-gray-app text-gray-normal">{props.children}</div>
+);
+
+render(
+  () => (
+    <Router root={AppLayout}>
+      <Route path="/" component={HomePage} />
+      <Route path="/list/:id" component={ListPage} />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement,
+);
