b06234d2d30e680f6498ed54fd3650a47920b7f7
jesseb34r <jesseb34r@jesseaubin.me>
2025-02-27 21:26:40 -0800

    use correct new kobalte import style

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,6 +1,10 @@
 import { createSignal, createUniqueId, Match, Switch } from "solid-js";
 import { useNavigate } from "@solidjs/router";
-import { Dialog, TextField, ToggleGroup } from "@kobalte/core";
+
+import { Button } from "@kobalte/core/button";
+import { Dialog } from "@kobalte/core/dialog";
+import { TextField } from "@kobalte/core/text-field";
+import { ToggleGroup } from "@kobalte/core/toggle-group";
 
 import { cards_parse_from_string, type CardList } from "../utils/card_list.ts";
 import { set_active_list } from "../index.tsx";
@@ -14,9 +18,7 @@ export function HomePage() {
 
   const navigate = useNavigate();
 
-  async function handle_create_list(e: Event) {
-    e.preventDefault();
-
+  async function handle_create_list() {
     const now = new Date().toISOString();
     const new_list: CardList = {
       id: createUniqueId(),
@@ -50,7 +52,7 @@ export function HomePage() {
       <div class="flex flex-col gap-2">
         <div class="flex gap-20 items-end justify-between">
           <h2 class="text-xl">Lists</h2>
-          <Dialog.Root open={is_import_dialog_open()} onOpenChange={set_is_import_dialog_open}>
+          <Dialog open={is_import_dialog_open()} onOpenChange={set_is_import_dialog_open}>
             <Dialog.Trigger
               onMouseDown={() => set_is_import_dialog_open(true)}
               class="
@@ -77,15 +79,11 @@ export function HomePage() {
                   onSubmit={handle_create_list}
                   class="flex flex-col items-stretch justify-center"
                 >
-                  <TextField.Root
-                    value={list_name()}
-                    onChange={set_list_name}
-                    class="flex flex-col"
-                  >
+                  <TextField value={list_name()} onChange={set_list_name} class="flex flex-col">
                     <TextField.Label>Name</TextField.Label>
                     <TextField.Input class="mb-4 bg-gray-7 dark:bg-graydark-7 px-1 rounded" />
-                  </TextField.Root>
-                  <ToggleGroup.Root
+                  </TextField>
+                  <ToggleGroup
                     value={import_from()}
                     onChange={set_import_from}
                     class="flex gap-2 mb-4"
@@ -120,18 +118,18 @@ export function HomePage() {
                     >
                       Import File
                     </ToggleGroup.Item>
-                  </ToggleGroup.Root>
+                  </ToggleGroup>
                   <div class="mb-4">
                     <Switch>
                       <Match when={import_from() === "paste"}>
-                        <TextField.Root
+                        <TextField
                           value={list_cards_pasted()}
                           onChange={set_list_cards_pasted}
                           class="flex flex-col"
                         >
                           <TextField.Label>Paste List</TextField.Label>
                           <TextField.TextArea class="bg-gray-7 dark:bg-graydark-7 px-1 rounded resize-none" />
-                        </TextField.Root>
+                        </TextField>
                       </Match>
                       <Match when={import_from() === "file"}>
                         <div class="flex flex-col">
@@ -152,9 +150,13 @@ export function HomePage() {
                       </Match>
                     </Switch>
                   </div>
-                  <button
-                    type="submit"
-                    value="submit"
+                  <Button
+                    onMouseDown={() => handle_create_list()}
+                    onKeyDown={(e: KeyboardEvent) => {
+                      if (e.key === "Enter" || e.key === " ") {
+                        handle_create_list();
+                      }
+                    }}
                     class="
                       px-2 py-1 rounded self-end cursor-pointer
                       bg-grass-4 dark:bg-grassdark-4
@@ -162,11 +164,11 @@ export function HomePage() {
                     "
                   >
                     Create
-                  </button>
+                  </Button>
                 </form>
               </Dialog.Content>
             </Dialog.Portal>
-          </Dialog.Root>
+          </Dialog>
         </div>
         <hr class="text-gray-dim" />
         <div>All my decks</div>
