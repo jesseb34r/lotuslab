aece9c33e0391d194c3fa1357516119409ce039d
jesseb34r <jesseb34r@jesseaubin.me>
2025-02-27 21:26:13 -0800

    use kobalte Link

--- src/pages/list_page.tsx
+++ src/pages/list_page.tsx
@@ -1,11 +1,16 @@
 import { A } from "@solidjs/router";
-import { active_list } from "../index.tsx";
 import { For, Show } from "solid-js";
+
+import { Link } from "@kobalte/core/link";
+
+import { active_list } from "../index.tsx";
 
 export function ListPage() {
   return (
     <div>
-      <A href="/">Home</A>
+      <Link as={A} href="/">
+        Home
+      </Link>
       <Show when={active_list()}>
         {(list) => <For each={list().cards}>{(card) => <div>{card.card.name}</div>}</For>}
       </Show>
